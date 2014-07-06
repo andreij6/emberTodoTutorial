7dd4323799084e0034ff8c9e061d1643f9c98700
andreij6 <andreij6@gmail.com>
2014-07-06 14:33:28 -0500

    adding child routes

--- emBlogReader/Scripts/router.js
+++ emBlogReader/Scripts/router.js
@@ -1,9 +1,18 @@
 ﻿Todos.Router.map(function () {
-    this.resource('todos', { path: '/' });
+    this.resource('todos', { path: '/' }, function () {
+        //additional child routes will go here later 
+        //outlet wouldnt work until I added at least an empty function here
+    });
 });
 
 Todos.TodosRoute = Ember.Route.extend({
     model: function () {
         return this.store.find('todo');
+    }
+});
+
+Todos.TodosIndexRoute = Ember.Route.extend({
+    model: function () {
+        return this.modelFor('todos');
     }
 });
