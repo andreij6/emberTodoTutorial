7df134507cd0679d155f37efd7e60aeca654d2c0
andreij6 <andreij6@gmail.com>
2014-07-06 11:48:32 -0500

    deleteing todo items completed

---
+++ emBlogReader/Scripts/views/edit_todo_view.js
@@ -0,0 +1,7 @@
+﻿Todos.EditTodoView = Ember.TextField.extend({
+    didInsertElement: function () {
+        this.$().focus();
+    }
+});
+
+Ember.Handlebars.helper('edit-todo', Todos.EditTodoView);
