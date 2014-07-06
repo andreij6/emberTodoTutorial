3148a2690b7b484a8a54a4ac6c05a723a7bff9ec
andreij6 <andreij6@gmail.com>
2014-07-05 19:03:51 -0500

    initial

---
+++ emBlogReader/Scripts/models/todo.js
@@ -0,0 +1,22 @@
+﻿Todos.Todo = DS.Model.extend({
+    title: DS.attr('string'),
+    isCompleted: DS.attr('boolean')
+});
+
+Todos.Todo.FIXTURES = [
+    {
+        id: 1,
+        title: 'Learn Ember.js',
+        isCompleted: true
+    },
+    {
+        id: 3,
+        title: 'Hand Coding This Tutorial',
+        isCompleted: false
+    },
+    {
+        id: 3,
+        title: 'Always Helps Me learn more!',
+        isCompleted: false
+    }
+]
