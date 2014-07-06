272630797381569a287bab1acc27495c45f60f2d
andreij6 <andreij6@gmail.com>
2014-07-05 21:49:32 -0500

    creating a new model instance -done

---
+++ emBlogReader/Scripts/controllers/todos_controller.js
@@ -0,0 +1,22 @@
+﻿Todos.TodosController = Ember.ArrayController.extend({
+    actions: {
+        createTodo: function () {
+            //get the todo title set by the New Todo text field
+            var title = this.get('newTitle');
+            if (!title) { return false; }
+            if (!title.trim()) { return; }
+
+            //Create the new todo model
+            var todo = this.store.createRecord('todo', {
+                title: title,
+                isCompleted: false
+            });
+
+            //clear the new todo text field
+            this.set('newTitle', '');
+
+            //save
+            todo.save();
+        }
+    }
+});
