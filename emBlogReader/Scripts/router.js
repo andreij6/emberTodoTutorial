ff61a6d265c83be4b233e7bdf9de83ced8a8eea0
andreij6 <andreij6@gmail.com>
2014-07-06 19:28:45 -0500

    implemented the links

--- emBlogReader/Scripts/router.js
+++ emBlogReader/Scripts/router.js
@@ -2,6 +2,8 @@
     this.resource('todos', { path: '/' }, function () {
         //additional child routes will go here later 
         //outlet wouldnt work until I added at least an empty function here
+        this.route('active');
+        this.route('completed');
     });
 });
 
@@ -14,5 +16,27 @@ Todos.TodosRoute = Ember.Route.extend({
 Todos.TodosIndexRoute = Ember.Route.extend({
     model: function () {
         return this.modelFor('todos');
+    }
+});
+
+Todos.TodosActiveRoute = Ember.Route.extend({
+    model: function () {
+        return this.store.filter('todo', function (todo) {
+            return !todo.get('isCompleted');
+        });
+    },
+    renderTemplate: function (controller) {
+        this.render('todos/index', { controller: controller });
+    }
+});
+
+Todos.TodosCompletedRoute = Ember.Route.extend({
+    model: function () {
+        return this.store.filter('todo', function (todo) {
+            return todo.get('isCompleted');
+        });
+    },
+    renderTemplate: function (controller) {
+        this.render('todos/index', { controller: controller });
     }
 });
