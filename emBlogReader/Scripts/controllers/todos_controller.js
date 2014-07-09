a72afaf20e6160f4304400170933228671fcc178
andreij6 <andreij6@gmail.com>
2014-07-08 22:30:07 -0500

    Finished

--- emBlogReader/Scripts/controllers/todos_controller.js
+++ emBlogReader/Scripts/controllers/todos_controller.js
@@ -44,6 +44,12 @@
     }.property('remaining'),
 
     allAreDone: function (key, value) {
-        return !!this.get('length') && this.isEvery('isCompleted');
+        if (value === undefined) {
+            return !!this.get('length') && this.everyProperty('isCompleted', true);
+        } else {
+            this.setEach('isCompleted', value);
+            this.invoke('save');
+            return value;
+        }
     }.property('@each.isCompleted')
 });
